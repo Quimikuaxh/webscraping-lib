b686fe8f476dc1e6508559f6e397265c74c233cb
Jose Gavilán <josegavilanruiz@gmail.com>
2024-12-24 15:20:52 +0100

    Added params to http request

--- src/utilities/scrap.ts
+++ src/utilities/scrap.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import xpath from 'xpath-html';
 
 class Scrap {
-    static async getHTML(url: string){
-        const response = await axios.get(url);
+    static async getHTML(url: string, params?){
+        const response = await axios.get(url, params);
         if (response.status != 200){
             throw new Error(`Data could not be retrieved from url ${url}. ` + response.statusText);
         }
